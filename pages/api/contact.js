981b11a2ff668764cfe993042f4cca19e4028a40
louisryan <louis.sw.ryan@gmail.com>
2023-07-19 08:40:21 +0200

    emails might be working

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,5 +1,3 @@
-// import { transporter } from "../../utils/nodeMailer";
-
 import nodemailer from "nodemailer";
 
 const handler = async (req, res) => {
@@ -21,43 +19,39 @@ const handler = async (req, res) => {
     try {
       await transporter.sendMail({
         from: "info.leasebreakersmelbourne@gmail.com",
-        to: 
-        // data.email
-        "louis.sw.ryan@gmail.com"
-        ,
+        to: data.email,
         subject: data.subject,
-        html: "simple string"
-      //   `
-      //   <!DOCTYPE html>
-      //   <html> 
-      //     <head> 
-      //       <title></title> <meta charset="utf-8"/> <meta name="viewport" content="width=device-width, initial-scale=1"/> <meta http-equiv="X-UA-Compatible" content="IE=edge"/> 
-      //     </head> 
-      //     <body style="margin: 0 !important; padding: 0 !important; background: #fff"> 
-      //       <div style=" display: none; font-size: 1px; color: #fefefe; line-height: 1px;  max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; " >
-      //         </div>
-      //           <table border="0" cellpadding="0" cellspacing="0" width="100%"> 
-      //             <tr> 
-      //               <td bgcolor="#ffffff" align="center" style="padding: 10px 15px 30px 15px" class="section-padding" > 
-      //                 <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 500px" class="responsive-table" > 
-      //                   <tr> 
-      //                     <td> 
-      //                       <table width="100%" border="0" cellspacing="0" cellpadding="0"> 
-      //                         <tr> 
-      //                           <td> 
-      //                             ${data.content}
-      //                           </td>
-      //                         </tr>
-      //                       </table> 
-      //                     </td>
-      //                   </tr>
-      //                 </table> 
-      //               </td>
-      //             </tr>
-      //           </table> 
-      //     </body>
-      //   </html>
-      // `
+        html: `
+          <!DOCTYPE html>
+          <html> 
+            <head> 
+              <title></title> <meta charset="utf-8"/> <meta name="viewport" content="width=device-width, initial-scale=1"/> <meta http-equiv="X-UA-Compatible" content="IE=edge"/> 
+            </head> 
+            <body style="margin: 0 !important; padding: 0 !important; background: #fff"> 
+              <div style=" display: none; font-size: 1px; color: #fefefe; line-height: 1px;  max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; " >
+                </div>
+                  <table border="0" cellpadding="0" cellspacing="0" width="100%"> 
+                    <tr> 
+                      <td bgcolor="#ffffff" align="center" style="padding: 10px 15px 30px 15px" class="section-padding" > 
+                        <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 500px" class="responsive-table" > 
+                          <tr> 
+                            <td> 
+                              <table width="100%" border="0" cellspacing="0" cellpadding="0"> 
+                                <tr> 
+                                  <td> 
+                                    ${data.content}
+                                  </td>
+                                </tr>
+                              </table> 
+                            </td>
+                          </tr>
+                        </table> 
+                      </td>
+                    </tr>
+                  </table> 
+            </body>
+          </html>
+        `
       });
 
       return res.status(200).json({ success: true, data: data });
